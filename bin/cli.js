a088484f6749418dee14028eb0b591b1e53634a7
keweiyan <keweiyan@live.unc.edu>
2022-11-17 02:17:00 -0500

    exported function

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,17 @@
+#!/usr/bin/env node
+
+// Dependencies
 import { roll } from "../lib/roll.js"
 import minimist from 'minimist'
 
 const args = minimist(process.argv.slice(2))
 
+// Declare sides, dices, rolls
 let sides = args.sides && args.sides > 0 ? args.sides: 6;
-let dice = args.dice && args.dice>0 ? args.dice: 2;
-let rolls = args.rolls && args.rolls>0 ? args.rolls: 1;
+let dices = args.dice && args.dice > 0 ? args.dice: 2;
+let rolls = args.rolls && args.rolls > 0 ? args.rolls: 1;
 
-console.log(JSON.stringify(roll(sides, dice, rolls)));
+// Output
+console.log(JSON.stringify(roll(sides, dices, rolls)));
+
+process.exit(0);
